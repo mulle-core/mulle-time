60004236a122eb7778ad2929491806ef1ff07af9
Nat! <nat-tv@mulle-kybernetik.com>
2019-01-15 17:38:11 +0100

    * modernized mulle-sde * UIEvents are distributed to subviews properly

--- .mulle/share/env/environment-plugin.sh
+++ .mulle/share/env/environment-plugin.sh
@@ -17,12 +17,12 @@ export MULLE_FETCH_SEARCH_PATH="${MULLE_VIRTUAL_ROOT}/.."
 #
 # Prefer symlinking to local git repositories found via MULLE_FETCH_SEARCH_PATH
 #
-export MULLE_SOURCETREE_SYMLINK='YES'
+export MULLE_SOURCETREE_SYMLINK="YES"
 
 #
 # Use common folder for sharable projects
 #
-export MULLE_SOURCETREE_SHARE_DIR="${MULLE_VIRTUAL_ROOT}/stash"
+export MULLE_SOURCETREE_STASH_DIRNAME="stash"
 
 #
 # Share dependency directory (absolute for ease of use)
@@ -38,9 +38,9 @@ export ADDICTION_DIR="${MULLE_VIRTUAL_ROOT}/addiction"
 # Use common build directory
 #
 export BUILD_DIR="${MULLE_VIRTUAL_ROOT}/build"
+#
+#
+#
+export MULLE_SDE_INSTALLED_VERSION="0.35.2"
 
-#
-# Modify path so that dependency and addictions executables are found and
-# preferred.
-#
-export PATH="${DEPENDENCY_DIR}/bin:${ADDICTION_DIR}/bin:$PATH"
+

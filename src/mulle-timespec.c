990a35a39b24fa1bf645c8039c4d702484668bcd
Nat! <nat@mulle-kybernetik.de>
2021-11-12 15:43:28 +0100

    fix for windows!

--- src/mulle-timespec.c
+++ src/mulle-timespec.c
@@ -11,6 +11,23 @@
 // Forcing everyone else to set _GNU_SOURCE who wants to include mulle-time.h
 // is misery.
 //
+#ifdef _WIN32
+
+#include <windows.h>
+
+// https://stackoverflow.com/questions/5404277/porting-clock-gettime-to-windows
+
+mulle_timeinterval_t   mulle_timeinterval_now( void)
+{  
+	__int64   wintime; 
+
+	GetSystemTimeAsFileTime((FILETIME*)&wintime);
+   wintime -=116444736000000000i64;  //1jan1601 to 1jan1970
+   return( (mulle_timeinterval_t) wintime / 10000000.0);
+}
+
+#else
+
 mulle_timeinterval_t   mulle_timeinterval_now( void)
 {
    struct timespec   now;
@@ -18,3 +35,5 @@ mulle_timeinterval_t   mulle_timeinterval_now( void)
    clock_gettime( CLOCK_REALTIME, &now);
    return( now.tv_sec + now.tv_nsec / (1000.0*1000*1000));
 }
+#endif
+

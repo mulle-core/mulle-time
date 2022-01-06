d1ec5538fcfa1eae453eebe4af2f952ef485e4d2
Nat! <nat@mulle-kybernetik.com>
2022-01-07 00:34:05 +0100

    up da version

--- RELEASENOTES.md
+++ RELEASENOTES.md
@@ -1,3 +1,8 @@
+## 0.1.0
+
+* Various small improvements
+
+
 ### 0.0.1
 
 * split mulle-time off from MulleUI
--- src/mulle-time.h
+++ src/mulle-time.h
@@ -1,7 +1,7 @@
 #ifndef mulle_time_h__
 #define mulle_time_h__
 
-#define MULLE_TIME_VERSION	((0 << 20) | (0 << 8) | 1)
+#define MULLE_TIME_VERSION	((0 << 20) | (1 << 8) | 0)
 
 
 #include "mulle-timetype.h"
